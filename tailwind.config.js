3df479677c849ca0183c6008a784cc8a93cb8b0c
Shadow Smith <shadow@businesscreditreports.com>
2019-09-25 16:01:13 -0500

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			white: '#FFF',
+			'gray-100': '#E5E5E5',
+			'gray-200': '#BDBDBD',
+			black: '#333',
+			'blue-100': '#F5FBFF',
+			'blue-600': '#2367A7',
+			'blue-800': '#2F5275'
+		}
+	},
+	variants: {
+		padding: ['hover']
+	},
+	plugins: []
 }
