5b44e48a7ecd1eac26df64c34b178d28258a91a2
Shadow Smith <shadow@businesscreditreports.com>
2019-09-26 15:41:47 -0500

    Create Sidebar sub component system

---
+++ components/layouts/dash/sidebar/SidebarLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <nuxt-link
+      class="sidebar__link px-4 block w-full text-white border-l-4 border-transparent hover:pl-5"
+      :to="destination"
+    >
+      <slot />
+    </nuxt-link>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    destination: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style>
+.sidebar__link-list .nuxt-link-exact-active {
+  @apply border-white;
+}
+
+.sidebar__link {
+  transition: padding 0.3s ease;
+}
+</style>
---
+++ components/layouts/dash/sidebar/SidebarLinkList.vue
@@ -0,0 +1,86 @@
+<template>
+  <ul class="sidebar__link-list flex flex-col py-3">
+    <SidebarLinkSection
+      v-for="section in sidebarLinkSections"
+      :key="section.id"
+      class="mb-4"
+      :section="section"
+    />
+  </ul>
+</template>
+
+<script>
+import SidebarLinkSection from './SidebarLinkSection'
+
+export default {
+  components: {
+    SidebarLinkSection
+  },
+  data() {
+    return {
+      sidebarLinkSections: [
+        {
+          id: 1,
+          name: 'Web Apps',
+          links: [
+            {
+              id: 1,
+              name: 'Email',
+              destination: '/email',
+              links: [
+                {
+                  id: 1,
+                  name: 'Inbox',
+                  destination: '/inbox'
+                }
+              ]
+            },
+            {
+              id: 2,
+              name: 'Chat',
+              destination: '/chat',
+              links: []
+            },
+            {
+              id: 3,
+              name: 'Calendar',
+              destination: '/calendar',
+              links: []
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: 'Hello',
+          links: [
+            {
+              id: 1,
+              name: 'Email',
+              destination: '/email',
+              links: [
+                {
+                  id: 1,
+                  name: 'Inbox',
+                  destination: '/inbox'
+                }
+              ]
+            },
+            {
+              id: 2,
+              name: 'Chat',
+              destination: '/chat',
+              links: []
+            },
+            {
+              id: 3,
+              name: 'Calendar',
+              destination: '/calendar',
+              links: []
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
