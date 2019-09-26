82151a2f60d6bfe5f61c479ad34f74b7da1ec8c6
Shadow Smith <shadow@businesscreditreports.com>
2019-09-26 15:41:04 -0500

    Add blue variations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,23 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-	theme: {
-		colors: {
-			transparent: 'transparent',
-			white: '#FFF',
-			'gray-100': '#E5E5E5',
-			'gray-200': '#BDBDBD',
-			black: '#333',
-			'blue-100': '#F5FBFF',
-			'blue-600': '#2367A7',
-			'blue-800': '#2F5275'
-		}
-	},
-	variants: {
-		padding: ['hover']
-	},
-	plugins: []
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#FFF',
+      'gray-100': '#E5E5E5',
+      'gray-200': '#BDBDBD',
+      black: '#333',
+      'blue-100': '#F5FBFF',
+      'blue-200': '#8cbff2',
+      'blue-400': '#569BDC',
+      'blue-600': '#2367A7',
+      'blue-800': '#2F5275',
+      'blue-900': '#0D273F'
+    }
+  },
+  variants: {
+    padding: ['hover']
+  },
+  plugins: []
 }
