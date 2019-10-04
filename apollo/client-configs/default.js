57d1e1a2d8af9dfdfa041f8ac018d1c3090d4583
ikey2244 <ikey2244@gmail.com>
2019-10-04 14:12:34 -0500

    Add persistent cache

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,9 +1,16 @@
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { persistCache } from 'apollo-cache-persist'
+
+const cache = new InMemoryCache()
+
+persistCache({
+  cache,
+  storage: window.localStorage
+})
 
 export default () => ({
   link: new HttpLink({ uri: 'http://localhost:4000/' }),
-  cache: new InMemoryCache(),
-  defaultHttpLink: false,
+  cache,
   connectDevTools: true
 })
