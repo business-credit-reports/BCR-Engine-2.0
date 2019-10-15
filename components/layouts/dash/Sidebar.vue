661c216220f803cd8f9dca0abb19bb0b57c3d01c
Shadow Smith <shadow@businesscreditreports.com>
2019-10-15 15:58:22 -0500

    Add sign out link to sidebar

--- components/layouts/dash/Sidebar.vue
+++ components/layouts/dash/Sidebar.vue
@@ -1,39 +1,50 @@
 <template>
-  <div class="sidebar bg-blue-600 shadow z-10">
-    <div class="flex justify-between py-3 px-6">
-      <Logo v-if="menuOpen" class="h-12 pr-6" />
-      <MenuIcon :open="menuOpen" class="py-3" @click.native="toggleMenu" />
-    </div>
-    <SidebarLinkList v-if="menuOpen" />
-  </div>
+	<div class="sidebar bg-blue-600 shadow z-10">
+		<div class="flex justify-between py-3 px-6">
+			<Logo v-if="menuOpen" class="h-12 pr-6" />
+			<MenuIcon :open="menuOpen" class="py-3" @click.native="toggleMenu" />
+		</div>
+		<SidebarLinkList v-if="menuOpen" />
+		<button
+			class="sidebar__link text-white border-l-4 border-transparent px-5 mb-2 hover:pl-6"
+			@click="signOut"
+		>
+			Signout
+		</button>
+	</div>
 </template>
 
 <script>
+import { Auth } from 'aws-amplify'
 import Logo from '../../Logo'
 import MenuIcon from '../../icons/MenuIcon'
 import SidebarLinkList from './SidebarLinkList'
 
 export default {
-  components: {
-    Logo,
-    MenuIcon,
-    SidebarLinkList
-  },
-  data() {
-    return {
-      menuOpen: true
-    }
-  },
-  methods: {
-    toggleMenu() {
-      this.menuOpen = !this.menuOpen
-    }
-  }
+	components: {
+		Logo,
+		MenuIcon,
+		SidebarLinkList
+	},
+	data() {
+		return {
+			menuOpen: true
+		}
+	},
+	methods: {
+		toggleMenu() {
+			this.menuOpen = !this.menuOpen
+		},
+		signOut() {
+			Auth.signOut()
+			this.$router.push({ path: '/' })
+		}
+	}
 }
 </script>
 
 <style>
 .sidebar {
-  grid-area: 1 / 1 / 4 / 2;
+	grid-area: 1 / 1 / 4 / 2;
 }
 </style>
