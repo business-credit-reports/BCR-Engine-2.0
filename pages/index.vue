acc193e66ba73102914401f217ae8edbd8c39c94
Shadow Smith <shadow@businesscreditreports.com>
2019-10-15 15:57:54 -0500

    Add amplify authenticator

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    Welcome
-  </div>
+	<div class="container">
+		<amplify-authenticator />
+	</div>
 </template>
 
 <script>
+import { AmplifyEventBus } from 'aws-amplify-vue'
+
 export default {
-  layout: 'dash'
+	layout: 'default',
+	data() {
+		return {
+			signedIn: false
+		}
+	},
+	created() {
+		AmplifyEventBus.$on('authState', (info) => {
+			if (info === 'signedIn') {
+				this.$router.push({ path: '/dashboard' })
+			}
+		})
+	}
 }
 </script>
+
+<style>
+.container {
+	margin: 0 auto;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+</style>
