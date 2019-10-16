1730cfebdb754c358ae54505b0ecd2337c9f3696
Shadow Smith <shadow@businesscreditreports.com>
2019-10-16 07:49:52 -0500

    Fix eslint spacing issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,37 @@
 <template>
-	<div class="container">
-		<amplify-authenticator />
-	</div>
+  <div class="container">
+    <amplify-authenticator />
+  </div>
 </template>
 
 <script>
 import { AmplifyEventBus } from 'aws-amplify-vue'
 
 export default {
-	layout: 'default',
-	data() {
-		return {
-			signedIn: false
-		}
-	},
-	created() {
-		AmplifyEventBus.$on('authState', (info) => {
-			if (info === 'signedIn') {
-				this.$router.push({ path: '/dashboard' })
-			}
-		})
-	}
+  layout: 'default',
+  data() {
+    return {
+      signedIn: false
+    }
+  },
+  created() {
+    AmplifyEventBus.$on('authState', (info) => {
+      if (info === 'signedIn') {
+        this.$router.push({ path: '/dashboard' })
+      }
+    })
+  }
 }
 </script>
 
 <style>
 .container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 </style>
