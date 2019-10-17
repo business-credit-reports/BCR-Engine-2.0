b848f691e0833f4ee498c922d91faa809f275e69
Shadow Smith <shadow@businesscreditreports.com>
2019-10-17 08:20:57 -0500

    Add sign up capability

--- components/layouts/default/auth/SignUp.vue
+++ components/layouts/default/auth/SignUp.vue
@@ -6,74 +6,128 @@
       stopColor="#314F8D"
       textColor="#2F5275"
     />
-    <h2 class="text-blue-800 text-2xl font-bold text-center">
-      Create a BCR App Engine account
-    </h2>
-    <p class="text-gray-200 mb-12 text-center">
-      Create an account to continue
-    </p>
-    <form>
-      <div class="mb-6">
-        <label class="text-black font-medium mb-2 block"
-          >Your organization name</label
-        >
-        <input
-          v-model="orgName"
-          class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
-          placeholder="Example, Inc."
-          type="text"
-        />
-      </div>
-      <div class="mb-6">
-        <label class="text-black font-medium mb-2 block">Your name</label>
-        <input
-          v-model="name"
-          class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
-          placeholder="John Smith"
-          type="text"
-        />
-      </div>
-      <div class="mb-6">
-        <label class="text-black font-medium mb-2 block">Your email</label>
-        <input
-          v-model="email"
-          class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
-          placeholder="john@exampleinc.com"
-          type="email"
-        />
-      </div>
-      <div class="mb-12">
-        <label class="text-black font-medium mb-2 block"
-          >Create your password</label
-        >
-        <input
-          v-model="password"
-          class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
-          placeholder="Password must have at least 8 characters"
-          type="password"
-        />
-      </div>
-      <button
-        class="w-full bg-blue-600 text-white py-3 rounded-lg mb-4"
-        type="button"
-        @click="signUp"
-      >
-        Create Account
-      </button>
-      <p class="text-center text-gray-200">
-        Already have an account?
-        <router-link class="text-blue-600" to="/signin">Login</router-link>
+    <div v-if="!confirmation">
+      <h2 class="text-blue-800 text-2xl font-bold text-center">
+        Create a BCR App Engine account
+      </h2>
+      <p class="text-gray-200 mb-12 text-center">
+        Create an account to continue
       </p>
-    </form>
+      <form>
+        <div class="mb-6">
+          <label class="text-black font-medium mb-2 block">Email</label>
+          <input
+            v-model="email"
+            class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
+            placeholder="john@exampleinc.com"
+            type="email"
+          />
+        </div>
+        <div class="mb-12">
+          <label class="text-black font-medium mb-2 block">Password</label>
+          <input
+            v-model="password"
+            class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
+            placeholder="Password must have at least 8 characters"
+            type="password"
+          />
+        </div>
+        <button
+          class="w-full bg-blue-600 text-white py-3 rounded-lg mb-4"
+          type="button"
+          @click.prevent="createAccount"
+        >
+          Create Account
+        </button>
+        <p class="text-center text-gray-200">
+          Already have an account?
+          <router-link class="text-blue-600" to="/signin">Login</router-link>
+        </p>
+      </form>
+    </div>
+    <div v-else>
+      <h2 class="text-blue-800 text-2xl font-bold text-center">
+        Confirm your email
+      </h2>
+      <p class="text-gray-200 mb-12 text-center">
+        Enter your confirmation code to continue
+      </p>
+      <form>
+        <div class="mb-12">
+          <label class="text-black font-medium mb-2 block"
+            >Confirmation Code</label
+          >
+          <input
+            v-model="code"
+            class="border border-gray-100 rounded-lg w-full p-3 leading-tight"
+            type="text"
+          />
+        </div>
+        <button
+          class="w-full bg-blue-600 text-white py-3 rounded-lg mb-4"
+          type="button"
+          @click="confirmEmail"
+        >
+          Confirm Email
+        </button>
+        <p class="text-center text-gray-200">
+          Didn't get the confirmation email?
+          <button
+            class="text-blue-600"
+            @click.prevent="resendConfirmationEmail"
+          >
+            Resend it
+          </button>
+        </p>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import { Auth } from 'aws-amplify'
 import Logo from '../../../Logo'
 
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      code: '',
+      confirmation: false
+    }
+  },
+  methods: {
+    createAccount() {
+      Auth.signUp({
+        username: this.email,
+        password: this.password
+      })
+        .then((data) => {
+          this.confirmation = true
+          console.log(data)
+        })
+        .catch((err) => console.log(err))
+    },
+    confirmEmail() {
+      Auth.confirmSignUp(this.email, this.code, {
+        forceAliasCreation: true
+      })
+        .then((data) => {
+          this.$router.push({ path: '/' })
+        })
+        .catch((err) => console.log(err))
+    },
+    resendConfirmationEmail() {
+      Auth.resendSignUp(this.email)
+        .then(() => {
+          console.log('Code resent successfully')
+        })
+        .catch((err) => console.log(err))
+    }
   }
 }
 </script>
