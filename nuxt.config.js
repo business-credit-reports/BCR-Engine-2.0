b16079dc956ce8a4257f1056dbc264a0c8ebb094
Shadow Smith <shadow@businesscreditreports.com>
2019-10-18 07:38:52 -0500

    Add aws-amplify to nuxt build config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,74 +1,75 @@
 export default {
-	mode: 'spa',
-	/*
-	 ** Headers of the page
-	 */
-	head: {
-		title: process.env.npm_package_name || '',
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{
-				hid: 'description',
-				name: 'description',
-				content: process.env.npm_package_description || ''
-			}
-		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-	},
-	/*
-	 ** Customize the progress-bar color
-	 */
-	loading: { color: '#2367A7' },
-	/*
-	 ** Global CSS
-	 */
-	css: [],
-	/*
-	 ** Plugins to load before mounting the App
-	 */
-	plugins: [{ src: '~/plugins/aws-amplify.js', mode: 'client' }],
-	/*
-	 ** Nuxt.js dev-modules
-	 */
-	buildModules: [
-		// Doc: https://github.com/nuxt-community/eslint-module
-		'@nuxtjs/eslint-module',
-		// Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-		'@nuxtjs/tailwindcss'
-	],
-	/*
-	 ** Nuxt.js modules
-	 */
-	modules: ['@nuxtjs/apollo'],
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#2367A7' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: '~/plugins/aws-amplify', mode: 'client' }],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/apollo'],
 
-	/*
+  /*
   ** Give apollo module options
   https://github.com/nuxt-community/apollo-module#setup
   */
 
-	apollo: {
-		defaultOptions: {
-			// See 'apollo' definition
-			// For example: default query options
-			$query: {
-				loadingKey: 'loading',
-				fetchPolicy: 'cache-and-network'
-			}
-		},
-		errorHandler: '~/plugins/apollo-error-handler.js',
+  apollo: {
+    defaultOptions: {
+      // See 'apollo' definition
+      // For example: default query options
+      $query: {
+        loadingKey: 'loading',
+        fetchPolicy: 'cache-and-network'
+      }
+    },
+    errorHandler: '~/plugins/apollo-error-handler.js',
 
-		clientConfigs: {
-			default: '~/apollo/client-configs/default.js'
-		}
-	},
-	/*
-	 ** Build configuration
-	 */
-	build: {
-		/*
-		 ** You can extend webpack config here
-		 */
-		extend(config, ctx) {}
-	}
+    clientConfigs: {
+      default: '~/apollo/client-configs/default.js'
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    vendor: ['aws-amplify'],
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
 }
